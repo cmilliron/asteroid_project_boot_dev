cbefbf0f03593d98337042f9eec832697b053f97
Cody Milliron <codymilliron.dev@gmail.com>
2025-11-10 21:37:14 -0500

    add shot and asteriod collision detection

--- main.py
+++ main.py
@@ -39,6 +39,11 @@ def main():
                 log_event("player_hit")
                 print("Game over")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
