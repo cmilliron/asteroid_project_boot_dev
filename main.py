56b9060ae4403b1bd17f470d1dacf879bd8912af
Cody Milliron <codymilliron.dev@gmail.com>
2025-11-10 21:31:52 -0500

    add players, asteroids, and ability to shoot

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+import sys
 
+player_start_x = SCREEN_WIDTH / 2
+player_start_y = SCREEN_HEIGHT / 2
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    player = Player(player_start_x, player_start_y)
+    asteroid_field = AsteroidField()
+    dt = 0
     while True:
+        dt = clock.tick(60) / 1000
         log_state()
         screen.fill("black")
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over")
+                sys.exit()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
